0a907a44af0fcbb554f46b14c6262b628f93c18c
Kayu <sullydoodler@gmail.com>
2019-10-20 15:29:58 -0700

    data folder

--- src/Document.java
+++ src/Document.java
@@ -25,23 +25,7 @@ public class Document {
     }
 
     private static String readFileAsString(String filename) {
-        Scanner scanner;
-        StringBuilder output = new StringBuilder();
-
-        try {
-            scanner = new Scanner(new FileInputStream(filename), "UTF-8");
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                output.append(line.trim() + "\n");
-            }
-
-            scanner.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found " + filename);
-        }
-
-        return output.toString();
+        return TextLib.readFileAsString(filename);
     }
 
     private String[] splitIntoWords() {
